0c9558573eb99bc55c3b7ca70bee2aa6cbcab107
Marcello Carvalhal <marcellocontact97@gmail.com>
2020-02-21 12:16:29 -0300

    first commit release v1.0

---
+++ README.md
@@ -0,0 +1,82 @@
+# Snake Game - Minha Versão
+
+  [![GitHub](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/marcelloc97/snake-game/blob/master/LICENSE)
+  ![](https://img.shields.io/badge/version-v1.0-red)
+  ![](https://img.shields.io/badge/languages-3-blue)
+  [![made-for-VSCode](https://img.shields.io/badge/Made%20for-VSCode-1f425f.svg)](https://code.visualstudio.com/)
+  ![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)
+
+  ## Índice
+
+  - [Descrição](#descrição)
+  - [Versões](#versões)
+  - [Game](#game)
+    - [Requisitos](#requisitos)
+    - [Bonus-tasks](#bonus-tasks)
+    - [Adicionais](#adicionais)
+    - [Como jogar?](#como-jogar)
+  
+  - [Meu GITHUB](#meu-github)
+
+  ## Descrição
+  Fui desafiado a criar, em curto prazo, um jogo. A proposta foi criar um 'snake game' básico, porém, minhas ideias continuam crescendo. \
+  Em breve criarei outras mecânicas e inclusive um modo **Multiplayer online**, será mais trabalhoso, porém divertido. \
+  Futuramente haverá mais atualizações no projeto.
+
+  ## Versões
+  Existe duas versões atualmente, ***singleplayer*** e ***two players***. \
+  A singleplayer estará disponível junto com os arquivos desse repositório. Pode ser acessado através da página principal! \
+  A multiplayer online estará disponível em breve no meu github.
+
+  ## Game
+
+  ### Requisitos
+
+    O que você precisa é só acessar [esta](https://marcelloc97.github.io/snake-game) página para jogar. \
+    Necessário conexão para acessar a página. \
+    Pontuações serão armazenadas no seu computador. Para ter acesso offline. \
+    **PS.: Apenas no navegador em um computador. Não disponível para Mobile.**
+
+    O jogo foi constituído com:
+  
+      * Arena. Simplesmente um espaço com, no mínimo, 10x10 pixels. ✅
+      * Cobra. Nossa cobra será o elemento que será controlado pelo jogador. Ela deverá ser composta por, pelo menos, um quadrado de 1x1 pixels. ✅
+      * Fruta. A fruta será o objetivo do jogador. Ela deverá ser feita por um quadrado de 1x1 pixels e com uma cor diferente do da Cobra. ✅
+
+    As regras são simples:
+    
+      * O jogador deverá controlar a cobra pela arena atrás da fruta. ✅
+      * Ao encostar na fruta, ela é consumida e o jogador ganha pontos, que devem ser exibidos em algum lugar na tela. ✅
+      * Sempre deve ter pelo menos uma fruta na arena, então, se o jogador consumir a fruta, outra deve ser gerada ao mesmo tempo. ✅
+
+  ### Bonus-tasks:
+  
+    As tarefas abaixo não são obrigatórias, mas serão diferenciais!
+
+      * A cobra cresce em 1 pixel a cada fruta comida. Os pixels subsequentes devem seguir o primeiro comando gerado. ✅
+      * Quando a cobra tenta avançar sobre os limites da arena, ela surge na direção oposta. ✅
+      * O jogo acaba quando a cobra encosta em alguma parte do seu próprio corpo. ✅
+      * O jogo está adaptado para interface mobile. ❌ (Ainda não!)
+      * Multi-jogador! É possível ter 2 jogadores ao mesmo tempo na tela competindo. Os jogadores não interagem entre si, somente comem as frutas. ✅
+
+  ### Adicionais:
+    * Highscore, uma pontuação máxima que fica registrada no localStorage do Browser.
+    * Efeitos sonoros, sim! Fica daora o jogo. Dentre esses sons tem um BGM (Background Music) e mais 3 sons referentes a consumo da Fruta, quando toca seu próprio corpo e Game Over.
+
+  ### Como jogar?
+  É bem simples. \
+  Ao jogar o modo *Singleplayer*, tudo que precisa fazer é mover a ~~cobrinha~~ (retângulo que se move...) e coletar as ~~frutas~~ (quadrados vermelhos). Para controlar a "cobrinha", utilize W, A, D ou S. \
+  \
+  Jogando o modo *Two players*, cada um controla uma "cobrinha". Uma verde e outra amarela. O objetivo é o mesmo, porém, vocês "competem" entre si. \
+  Mas um **detalhe**!!! Se um morrer, começa tudo de novo. ***Um desafio a mais para vocês***! Para controlar suas "cobrinhas", o jogador nº 1 usa W, A, D ou S, o jogador nº 2 usa as setas! \
+  \
+  Fácil não é?
+
+
+
+# Meu GITHUB
+  O repositório está livre! Use **``git clone https://github.com/marcelloc97/snake-game``** para poder editar. \
+  O código ficará aberto para quem quiser contribuir.
+
+
+# Muito obrigado e um forte abraço!
---
+++ src/js/game.js
@@ -0,0 +1,453 @@
+var WIDTH, HEIGHT; // control canvas size
+
+var score = (highscore = 0);
+var score2 = (highscore2 = 0);
+
+// player 1
+const vel = 1; // constant velocity
+var px = Math.floor(Math.random() * 35),
+  py = Math.floor(Math.random() * 30); // player's head position
+var vx = 1, // player body position
+  vy = 0;
+var trail = []; // his tail pixels
+var tail = 2; // quantity of tails
+var lost = false; // if touched his body / win not implemented... YET
+
+// player 2
+var px2 = Math.floor(Math.random() * 35),
+  py2 = Math.floor(Math.random() * 30); // player's head position
+var vx2 = 1, // player body position
+  vy2 = 0;
+var trail2 = []; // his tail pixels
+var tail2 = 2; // quantity of tails
+var lost2 = false; // if touched his body / win not implemented... YET
+
+// Fruit position
+var fx = Math.floor(Math.random() * 35),
+  fy = Math.floor(Math.random() * 30);
+
+// Sound Effects
+const fruitSFX = new Audio("src/assets/audio/sfx/Fruit.wav");
+const gameOverSFX = new Audio("src/assets/audio/sfx/Game_Over.ogg");
+const hurtSFX = new Audio("src/assets/audio/sfx/Hurt.wav");
+
+// Background Music
+const stageBGM = new Audio("src/assets/audio/bgm/bgm01.mp3");
+stageBGM.volume = 0.4;
+
+var canPlay = true; // used for gameOverSFX
+
+var scoreboard = document.getElementById("scoreboard"); // the DOM element below page's title
+var scoreboard2 = document.getElementById("scoreboard2"); // the DOM element below page's title
+var previousScore = 0; // ah.. the previous score?
+var previousScore2 = 0; // ah.. the previous score?
+
+var scoreString = "P1 Score : " + score;
+var highscoreString =
+  "P1 Highscore : " + localStorage.getItem("snake-game.highscore");
+
+var scoreString2 = "<br />P2 Score : " + score2;
+var highscoreString2 =
+  "P2 Highscore : " + localStorage.getItem("snake-game.highscore-p2");
+
+var time = 100; // time in milliseconds
+
+//// Observation: Player wins when score gets to 100 (but not yet)
+
+window.onload = function initialize() {
+  WIDTH = 35;
+  HEIGHT = 30;
+
+  canvas = document.getElementById("game-canvas");
+  ctx = canvas.getContext("2d");
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  canvas.style.width = `${WIDTH * 18}px`;
+  canvas.style.height = `${HEIGHT * 18}px`;
+
+  document.addEventListener("keydown", keyPush);
+
+  // gets highscore from localStorage
+  if (localStorage.getItem("snake-game.highscore") != null) {
+    highscore = Number.parseInt(localStorage.getItem("snake-game.highscore"));
+  } else {
+    localStorage.setItem("snake-game.highscore", highscore.toString());
+  }
+
+  // P2
+  if (localStorage.getItem("snake-game.highscore-p2") != null) {
+    highscore2 = Number.parseInt(
+      localStorage.getItem("snake-game.highscore-p2")
+    );
+  } else {
+    localStorage.setItem("snake-game.highscore-p2", highscore2.toString());
+  }
+
+  // Shows score and highscore on top of the game canvas
+  var sr = document.createElement("h2");
+  var hsr = document.createElement("h4");
+
+  sr.innerHTML = `${scoreString}`;
+  sr.style.textAlign = "center";
+  sr.style.marginRight = "10px";
+  sr.id = "score-info";
+
+  hsr.innerHTML = `${highscoreString}`;
+  hsr.style.textAlign = "center";
+  hsr.style.marginRight = "10px";
+  hsr.id = "score-info";
+
+  scoreboard.appendChild(sr);
+  scoreboard.appendChild(hsr);
+
+  // P2
+  var sr2 = document.createElement("h2");
+  var hsr2 = document.createElement("h4");
+
+  sr2.innerHTML = `${scoreString2}`;
+  sr2.style.textAlign = "center";
+  sr2.style.marginRight = "10px";
+  sr2.id = "score-info";
+
+  hsr2.innerHTML = `${highscoreString2}`;
+  hsr2.style.textAlign = "center";
+  hsr2.style.marginRight = "10px";
+  hsr2.id = "score-info";
+
+  scoreboard2.appendChild(sr2);
+  scoreboard2.appendChild(hsr2);
+
+  restartGame(); // fix audio bug
+
+  setInterval(run, time);
+};
+
+// Application Runner
+function run() {
+  update();
+  render();
+}
+
+// Game Updater Function
+function update() {
+  // Snake Logic P1
+  px += vx;
+  py += vy;
+
+  // Arena Wrap
+  if (px < 0) {
+    px = WIDTH - 1;
+  }
+  if (px > WIDTH - 1) {
+    px = 0;
+  }
+  if (py < 0) {
+    py = HEIGHT - 1;
+  }
+  if (py > HEIGHT - 1) {
+    py = 0;
+  }
+
+  // Snake Logic P2
+  px2 += vx2;
+  py2 += vy2;
+
+  // Arena Wrap
+  if (px2 < 0) {
+    px2 = WIDTH - 1;
+  }
+  if (px2 > WIDTH - 1) {
+    px2 = 0;
+  }
+  if (py2 < 0) {
+    py2 = HEIGHT - 1;
+  }
+  if (py2 > HEIGHT - 1) {
+    py2 = 0;
+  }
+
+  lost ? stop(stageBGM) : play(stageBGM);
+
+  canvas.onclick = restartGame;
+}
+
+// Game Render Function
+function render() {
+  ctx.fillStyle = "#FFF"; //"#50b0FF";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  ctx.fillStyle = "#AA0000";
+  ctx.fillRect(fx, fy, 1, 1);
+
+  // P1
+  ctx.fillStyle = "#0d5959"; //"#22CC00"; //"#444FFF";
+  for (var i = 0; i < trail.length; i++) {
+    ctx.fillRect(trail[i].x, trail[i].y, 1, 1);
+
+    // Lose (Touched your own body)
+    if (trail[i].x == px && trail[i].y == py) {
+      // Reset Snake stats
+      vx = vy = 0;
+      lost = true;
+
+      // Play Game Over Sound
+      if (canPlay && lost) {
+        play(hurtSFX);
+        play(gameOverSFX);
+        canPlay = false;
+      } else if (canPlay && !lost) {
+        stop(gameOverSFX);
+        return;
+      }
+
+      if (score > highscore) {
+        highscore = score;
+
+        localStorage.setItem("snake-game.highscore", highscore.toString());
+      }
+    }
+  }
+
+  trail.push({ x: px, y: py });
+  while (trail.length > tail) {
+    trail.shift();
+  }
+
+  // Gets a Fruit
+  if (fx == px && fy == py) {
+    play(fruitSFX);
+    tail++;
+    fx = Math.floor(Math.random() * WIDTH);
+    fy = Math.floor(Math.random() * HEIGHT);
+    score++;
+  }
+
+  // P2
+  ctx.fillStyle = "#ffcc00"; //"#228800"; //"#444FFF";
+  for (var i = 0; i < trail2.length; i++) {
+    ctx.fillRect(trail2[i].x, trail2[i].y, 1, 1);
+
+    // Lose (Touched your own body)
+    if (trail2[i].x == px2 && trail2[i].y == py2) {
+      // Reset Snake stats
+      vx2 = vy2 = 0;
+      lost2 = true;
+
+      // Play Game Over Sound
+      if (canPlay && lost2) {
+        play(hurtSFX);
+        play(gameOverSFX);
+        canPlay = false;
+      } else if (canPlay && !lost2) {
+        stop(gameOverSFX);
+        return;
+      }
+
+      if (score2 > highscore2) {
+        highscore2 = score2;
+
+        localStorage.setItem("snake-game.highscore-p2", highscore2.toString());
+      }
+    }
+  }
+
+  trail2.push({ x: px2, y: py2 });
+  while (trail2.length > tail2) {
+    trail2.shift();
+  }
+
+  // Gets a Fruit
+  if (fx == px2 && fy == py2) {
+    play(fruitSFX);
+    tail2++;
+    fx = Math.floor(Math.random() * WIDTH);
+    fy = Math.floor(Math.random() * HEIGHT);
+    score2++;
+  }
+
+  // Update strings
+  scoreString = "P1 Score : " + score;
+  score > highscore
+    ? (highscoreString = "P1 Highscore : " + score)
+    : (highscoreString =
+        "P1 Highscore : " + localStorage.getItem("snake-game.highscore"));
+
+  // Update DOM elements
+  if (previousScore < score || previousScore === score) {
+    var sr = document.createElement("h3");
+    var hsr = document.createElement("h4");
+    sr.innerHTML = `${scoreString}`;
+    sr.style.textAlign = "center";
+    sr.style.marginRight = "10px";
+    sr.id = "score-info";
+
+    hsr.innerHTML = `${highscoreString}`;
+    hsr.style.textAlign = "center";
+    hsr.style.marginRight = "10px";
+    hsr.id = "score-info";
+
+    if (score > highscore) {
+      hsr.style.color = "#0d5959";
+    } else {
+      hsr.style.color = "#FFF";
+    }
+
+    scoreboard.removeChild(scoreboard.firstChild); // Score: ...
+    scoreboard.removeChild(scoreboard.firstChild); // Highscore: ...
+    scoreboard.appendChild(sr);
+    scoreboard.appendChild(hsr);
+
+    previousScore = score;
+  }
+
+  // Update strings P2
+  scoreString2 = "<br />P2 Score : " + score2;
+  score2 > highscore2
+    ? (highscoreString2 = "P2 Highscore : " + score2)
+    : (highscoreString2 =
+        "P2 Highscore : " + localStorage.getItem("snake-game.highscore-p2"));
+
+  // Update DOM elements P2
+  if (previousScore2 < score2 || previousScore2 === score2) {
+    var sr2 = document.createElement("h3");
+    var hsr2 = document.createElement("h4");
+    sr2.innerHTML = `${scoreString2}`;
+    sr2.style.textAlign = "center";
+    sr2.style.marginRight = "10px";
+    sr2.id = "score-info";
+
+    hsr2.innerHTML = `${highscoreString2}`;
+    hsr2.style.textAlign = "center";
+    hsr2.style.marginRight = "10px";
+    hsr2.id = "score-info";
+
+    if (score2 > highscore2) {
+      hsr2.style.color = "#ffcc00"; //"#C8F225";
+    } else {
+      hsr2.style.color = "#FFF";
+    }
+
+    scoreboard2.removeChild(scoreboard2.firstChild); // Score: ...
+    scoreboard2.removeChild(scoreboard2.firstChild); // Highscore: ...
+    scoreboard2.appendChild(sr2);
+    scoreboard2.appendChild(hsr2);
+
+    previousScore2 = score2;
+  }
+
+  // Change Screen when player has lost
+  if (lost || lost2) {
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    ctx.fillStyle = "#FFF";
+    ctx.fontSize = "5px";
+    ctx.fillText("K.O.", 9, 17);
+  }
+}
+
+// Input Controller
+function keyPush(event) {
+  switch (event.keyCode) {
+    case 37: // Arrow Left P2
+      if (!lost2) {
+        vx2 = -vel;
+        vy2 = 0;
+      }
+      break;
+    case 65: // A key P1
+      if (!lost) {
+        vx = -vel;
+        vy = 0;
+      }
+      break;
+    case 38: // up P2
+      if (!lost2) {
+        vx2 = 0;
+        vy2 = -vel;
+      }
+      break;
+    case 87: // W key P1
+      if (!lost) {
+        vx = 0;
+        vy = -vel;
+      }
+      break;
+    case 39: // right P2
+      if (!lost2) {
+        vx2 = vel;
+        vy2 = 0;
+      }
+      break;
+    case 68: // D key P1
+      if (!lost) {
+        vx = vel;
+        vy = 0;
+      }
+      break;
+    case 40: // down P2
+      if (!lost2) {
+        vx2 = 0;
+        vy2 = vel;
+      }
+      break;
+    case 83: // S key P1
+      if (!lost) {
+        vx = 0;
+        vy = vel;
+      }
+      break;
+
+    case 13: // enter
+    case 27: // escape
+    case 32: // space
+      restartGame();
+      break;
+  }
+}
+
+function restartGame() {
+  if (lost || lost2) {
+    tail = 2;
+    px = py = Math.floor(Math.random() * WIDTH);
+    score = 0;
+    vx = 1;
+    vy = 0;
+    lost = false;
+    previousScore = 0;
+
+    // P2
+    tail2 = 2;
+    px2 = py2 = Math.floor(Math.random() * WIDTH);
+    score2 = 0;
+    vx2 = 1;
+    vy2 = 0;
+    lost2 = false;
+    previousScore2 = 0;
+
+    fx = fy = Math.floor(Math.random() * HEIGHT);
+    canPlay = true;
+  }
+}
+
+// Reusable Functions
+function play(audio) {
+  audio.play();
+}
+
+function stop(audio) {
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+function isPlaying(audio) {
+  if (audio.played) {
+    return true;
+  } else return false;
+}
+
+// External Functions
+function restart() {
+  restartGame();
+}
